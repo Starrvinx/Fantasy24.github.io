dda06acb3b3d8bff78ecf7d8074e0b617b63ed34
Starrvinx <rainjustine2310@gmail.com>
2025-08-24 22:23:35 +0800

    Add files via upload

---
+++ 21_Manuel_Assignment3/Developer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <title> About Minecraft  </title>
+    
+<head>
+
+    <link rel='stylesheet' type="text/css" href="mystyles.css">
+
+<style>
+</style>
+
+
+
+</head>
+
+<body>
+</body>
+
+<div class="header">
+<h1> Developer </h1>
+<p> Short introduction  <p>
+</div>
+
+
+</div>
+
+<div class="nav">
+    <ul> 
+    <li><a href="index.php"> Home </a></li>
+    <li><a href="News.php"> News </a></li>
+    <li><a href="Store.php"> Store </a></li>
+    <li><a href="Developer.php"> Developer </a></li>
+    </ul>
+</div>
+
+
+<div class="Developer">
+
+    <h2>About The Developer</h2>
+    <image src="images/developer.jpg" alt="Developer Image" class="Developer-Photo"> 
+    <p>Hello! My name Is <strong>Rain Justine Q. Manuel</strong>, a passionate and aspiring web developer.
+    I enjoy buildign websites using <em> HTML, CSS, PHP, and MySQL</em>
+    I created this webpage as part of my learning journey in web development.
+    My goal is to design user-friendly and fucntional websites that can help solve real world problems.
+    In the future, I hope to become a full-stack web developer and work on exciting teach projects that make a difference
+     </p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="footer">
+    All rights reserved &copy; 2025 By Rain Justine Q. Manuel, Developer and Designer of this website.
+</div>
+
+</html>
